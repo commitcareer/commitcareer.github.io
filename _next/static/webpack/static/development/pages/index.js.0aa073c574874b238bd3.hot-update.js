49053312fc24b79187394737555a3033a8311dc3
Ted Park <itstedpark@gmail.com>
2019-10-28 16:05:07 +0900

    add update

---
+++ _next/static/webpack/static/development/pages/index.js.0aa073c574874b238bd3.hot-update.js
@@ -0,0 +1,190 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Index; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _material_ui_core_Container__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @material-ui/core/Container */ "./node_modules/@material-ui/core/esm/Container/index.js");
+/* harmony import */ var _material_ui_core_Typography__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @material-ui/core/Typography */ "./node_modules/@material-ui/core/esm/Typography/index.js");
+/* harmony import */ var _material_ui_core_Box__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @material-ui/core/Box */ "./node_modules/@material-ui/core/esm/Box/index.js");
+/* harmony import */ var _material_ui_core_Link__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @material-ui/core/Link */ "./node_modules/@material-ui/core/esm/Link/index.js");
+/* harmony import */ var _src_ProTip__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../src/ProTip */ "./src/ProTip.js");
+/* harmony import */ var _src_Link__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../src/Link */ "./src/Link.js");
+/* harmony import */ var _material_ui_core_Grid__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! @material-ui/core/Grid */ "./node_modules/@material-ui/core/esm/Grid/index.js");
+/* harmony import */ var _material_ui_core_styles__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! @material-ui/core/styles */ "./node_modules/@material-ui/core/esm/styles/index.js");
+var _jsxFileName = "/Users/ted/bitlocal/resume_nextjs/resume_nextjs/pages/index.js";
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+
+
+
+
+
+
+
+var imageArray = ["https://is1-ssl.mzstatic.com/image/thumb/Purple123/v4/56/2e/d4/562ed45c-daec-3596-005a-2d34c357370f/pr_source.png/0x0ss-P3.jpg", "https://is1-ssl.mzstatic.com/image/thumb/Purple113/v4/12/91/93/1291930d-ee95-e5bb-a276-dbba95b23c7a/pr_source.png/0x0ss-P3.jpg", "https://is1-ssl.mzstatic.com/image/thumb/Purple113/v4/d9/09/b5/d909b5dd-814d-1d02-aaed-1557efffda8a/pr_source.png/0x0ss-P3.jpg", "https://is1-ssl.mzstatic.com/image/thumb/Purple113/v4/05/e9/c2/05e9c22a-4ece-8f2d-060b-d87f1471dfc7/pr_source.png/0x0ss-P3.jpg", "https://is1-ssl.mzstatic.com/image/thumb/Purple123/v4/cd/99/a1/cd99a132-42e2-2f79-fd52-4cda2312aa0f/pr_source.png/0x0ss-P3.jpg", "https://is1-ssl.mzstatic.com/image/thumb/Purple113/v4/e7/70/30/e7703054-1ab5-163c-0119-1d2acc2457ad/Icon-60@2x.png.png/0x0ss.png"];
+var useStyles = Object(_material_ui_core_styles__WEBPACK_IMPORTED_MODULE_8__["makeStyles"])(function (theme) {
+  return {
+    root: {
+      padding: theme.spacing(3, 2) // justifyContent: "center"
+
+    },
+    main: {
+      padding: theme.spacing(3, 2),
+      // justifyContent: "center"
+      backgroundColor: "#333333"
+    },
+    // 212121
+    imageBorder: {
+      borderRadius: 0
+    },
+    paperStyle: {
+      padding: theme.spacing(3, 2),
+      backgroundColor: "#212121"
+    }
+  };
+});
+
+function Copyright() {
+  return __jsx(_material_ui_core_Typography__WEBPACK_IMPORTED_MODULE_2__["default"], {
+    variant: "body2",
+    color: "textSecondary",
+    align: "center",
+    style: {
+      color: "#757575"
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 42
+    },
+    __self: this
+  }, "Copyright © ", __jsx(_material_ui_core_Link__WEBPACK_IMPORTED_MODULE_4__["default"], {
+    color: "inherit",
+    href: "https://commitcareer.github.io/",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 49
+    },
+    __self: this
+  }, "CommitCareer"), " ", new Date().getFullYear(), ".");
+}
+
+function Index() {
+  var classes = useStyles();
+  return __jsx(_material_ui_core_Grid__WEBPACK_IMPORTED_MODULE_7__["default"], {
+    style: {
+      backgroundColor: "#333333"
+    },
+    container: true,
+    justify: "space-evenly",
+    className: classes.main,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 61
+    },
+    __self: this
+  }, __jsx("div", {
+    style: {
+      backgroundColor: "#333333"
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 69
+    },
+    __self: this
+  }, __jsx(_material_ui_core_Grid__WEBPACK_IMPORTED_MODULE_7__["default"], {
+    style: {
+      backgroundColor: "#333333"
+    },
+    container: true,
+    direction: "row",
+    justify: "space-around",
+    alignItems: "center",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 74
+    },
+    __self: this
+  }, __jsx(_src_Link__WEBPACK_IMPORTED_MODULE_6__["default"], {
+    href: "/okrdoit",
+    color: "secondary",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 83
+    },
+    __self: this
+  }, __jsx("img", {
+    style: {
+      border: "2px solid #757575",
+      borderRadius: 5,
+      mar: mar
+    },
+    width: 185,
+    src: imageArray[0],
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 84
+    },
+    __self: this
+  })), __jsx(_src_Link__WEBPACK_IMPORTED_MODULE_6__["default"], {
+    href: "/sharebooks",
+    color: "secondary",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 96
+    },
+    __self: this
+  }, __jsx("img", {
+    style: {
+      border: "2px solid #757575",
+      borderRadius: 5
+    },
+    width: 185,
+    src: "https://is1-ssl.mzstatic.com/image/thumb/Purple128/v4/cd/6d/d1/cd6dd1a3-b52d-e6c1-5ced-dc5a51ba5e40/mzl.hstfhsdf.png/0x0ss-P3.jpg",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 97
+    },
+    __self: this
+  }))), __jsx("div", {
+    style: {
+      backgroundColor: "#333333",
+      position: "absolute",
+      left: 0,
+      bottom: 0,
+      right: 0
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 109
+    },
+    __self: this
+  }, __jsx(_material_ui_core_Box__WEBPACK_IMPORTED_MODULE_3__["default"], {
+    my: 2,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 118
+    },
+    __self: this
+  }, __jsx(Copyright, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 119
+    },
+    __self: this
+  })))));
+}
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.0aa073c574874b238bd3.hot-update.js.map
